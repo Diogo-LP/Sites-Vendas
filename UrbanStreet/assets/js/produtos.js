29054cad72c576d486470e18807616cfc4f99c9a
Diogo-LP <140905190+Diogo-LP@users.noreply.github.com>
2023-11-07 16:43:38 -0300

    MDSSSSSSS
    consegui, sou foda :)

--- UrbanStreet/assets/js/produtos.js
+++ UrbanStreet/assets/js/produtos.js
@@ -1,55 +1,55 @@
 var camisetas = [
     {
         nome: 'CAMISA URBAN "SINCE PIXO"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD1.jpg",
         imgtras: "assets/img/camisas/MOD1TRAS.jpg",
         cor: "p"
     },  {
         nome: 'CAMISA URBAN "NINGUÉM TA PURO"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD2.jpg",
         imgtras: "assets/img/camisas/MOD2TRAS.jpg",
         cor: "p"    
     }, {
         nome: 'CAMISA URBAN "GRAFITE URBAN"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD9.jpg",
         imgtras: "assets/img/camisas/MOD9TRAS.jpg",
         cor: "p"
     }, {
         nome: 'CAMISA URBAN "CENA DO CRIME"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD6.jpg",
         imgtras: "assets/img/camisas/MOD6TRAS.jpg",
         cor: "p"
     }, {
         nome: 'CAMISA URBAN "UNDER ZOO"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD3.jpg",
         imgtras: "assets/img/camisas/MOD3TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "BANDIDA"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD4.jpg",
         imgtras: "assets/img/camisas/MOD4TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA BÁSICA URBAN ROXO',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD5.jpg",
         imgtras: "assets/img/camisas/MOD5TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "CYBER URBAN"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD7.jpg",
         imgtras: "assets/img/camisas/MOD7TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "URBAN BUSS"',
-        preco: "109,9",
+        preco: "102,82",
         img: "assets/img/camisas/MOD8.jpg",
         imgtras: "assets/img/camisas/MOD8TRAS.jpg",
         cor: "b"
@@ -57,32 +57,26 @@ var camisetas = [
 ]
 var calcas = [
     {
-
+        nome: 'CALÇA CARGO',
+        preco: '155,94',
+        img: "assets/img/calcas/calcacargo.jpg",
+        imgtras: "assets/img/calcas/calcacargo.jpg",
+        cor: "p"
     }
 ]
 
 var moletons = [
     {
         nome: 'MOLETOM URBAN "HIP HOP BOY"',
-        preco: '149,9',
+        preco: '145,00',
         img: "assets/img/moletons/MOD1.jpg",
         imgtras: "assets/img/moletons/MOD1COSTAS.jpg",
         cor: "p"
     }, {
         nome: 'MOLETOM URBAN "WILDSTYLE"',
-        preco: '149,9',
+        preco: '145,00',
         img: "assets/img/moletons/MOD2.jpg",
         imgtras: "assets/img/moletons/MOD2.jpg",
-        cor: "p"
-    }
-]
-
-var bones = [
-    {
-        nome: 'BONÉ URBAN FIVE PANEL CORDÃO',
-        preco: '59,9',
-        img: "assets/img/bones/bone1.jpg",
-        imgtras: "assets/img/bones/bone1.jpg",
         cor: "p"
     }
 ]
