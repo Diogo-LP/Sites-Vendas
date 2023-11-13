0a1ef8e64e87a25b0b1c9b8c927583fb3a032eb3
Felipovisk22 <felipeggoncales@gmail.com>
2023-11-13 17:26:43 -0300

    calculadora

--- UrbanStreet/assets/js/produtos.js
+++ UrbanStreet/assets/js/produtos.js
@@ -1,55 +1,55 @@
 var camisetas = [
     {
         nome: 'CAMISA URBAN "SINCE PIXO"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD1.jpg",
         imgtras: "assets/img/camisas/MOD1TRAS.jpg",
         cor: "p"
     },  {
         nome: 'CAMISA URBAN "NINGUÉM TA PURO"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD2.jpg",
         imgtras: "assets/img/camisas/MOD2TRAS.jpg",
         cor: "p"    
     }, {
         nome: 'CAMISA URBAN "GRAFITE URBAN"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD9.jpg",
         imgtras: "assets/img/camisas/MOD9TRAS.jpg",
         cor: "p"
     }, {
         nome: 'CAMISA URBAN "CENA DO CRIME"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD6.jpg",
         imgtras: "assets/img/camisas/MOD6TRAS.jpg",
         cor: "p"
     }, {
         nome: 'CAMISA URBAN "UNDER ZOO"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD3.jpg",
         imgtras: "assets/img/camisas/MOD3TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "BANDIDA"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD4.jpg",
         imgtras: "assets/img/camisas/MOD4TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA BÁSICA URBAN ROXO',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD5.jpg",
         imgtras: "assets/img/camisas/MOD5TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "CYBER URBAN"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD7.jpg",
         imgtras: "assets/img/camisas/MOD7TRAS.jpg",
         cor: "b"
     },{
         nome: 'CAMISA URBAN "URBAN BUSS"',
-        preco: "102,82",
+        preco: "109,90",
         img: "assets/img/camisas/MOD8.jpg",
         imgtras: "assets/img/camisas/MOD8TRAS.jpg",
         cor: "b"
@@ -58,7 +58,7 @@ var camisetas = [
 var calcas = [
     {
         nome: 'CALÇA CARGO',
-        preco: '155,94',
+        preco: '169,90',
         img: "assets/img/calcas/calcacargo.jpg",
         imgtras: "assets/img/calcas/calcacargo.jpg",
         cor: "p"
@@ -68,13 +68,13 @@ var calcas = [
 var moletons = [
     {
         nome: 'MOLETOM URBAN "HIP HOP BOY"',
-        preco: '145,00',
+        preco: '149,90',
         img: "assets/img/moletons/MOD1.jpg",
         imgtras: "assets/img/moletons/MOD1COSTAS.jpg",
         cor: "p"
     }, {
         nome: 'MOLETOM URBAN "WILDSTYLE"',
-        preco: '145,00',
+        preco: '149,90',
         img: "assets/img/moletons/MOD2.jpg",
         imgtras: "assets/img/moletons/MOD2.jpg",
         cor: "p"
